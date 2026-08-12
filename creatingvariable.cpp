940fc0d91be6b5755e5d188dbac005d818efa6e1
Garvit Jaiswal <garvitjaiswal12@gmail.com>
2026-08-12 17:58:23 +0530

    Create creatingvariable.cpp

---
+++ creatingvariable.cpp
@@ -0,0 +1,12 @@
+ #include <iostream>
+using namespace std;
+int main() {
+    int age=19;
+    float height;
+    bool isit;
+    
+    char sex='m';
+    height=8.23;
+    cout<<"age "<<age<<endl<<"height "<<height<<endl<<"sex "<<sex<<endl<< "isit "<<isit<<endl;
+    return 0;
+}
