a6065f8dc5bc5460afc2d840f51941c1ff4da883
Garvit Jaiswal <garvitjaiswal12@gmail.com>
2026-08-12 18:12:52 +0530

    Create cin.cpp

---
+++ cin.cpp
@@ -0,0 +1,10 @@
+ #include <iostream>
+using namespace std;
+int main() {
+    int age;
+    float height;
+    cout<<"enter your age and height";
+    cin>>age>>height;
+    cout<<"age "<<age<<endl<<"height "<<height <<endl;
+    return 0;
+}
