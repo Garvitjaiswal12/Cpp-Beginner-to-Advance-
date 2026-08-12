05c3d59c62323bd42fae81eb993617af6bf4ca9c
Garvit Jaiswal <garvitjaiswal12@gmail.com>
2026-08-12 23:05:08 +0530

    Create nesetedif-else.cpp

---
+++ nesetedif-else.cpp
@@ -0,0 +1,41 @@
+ #include <iostream>
+
+using namespace std;
+int main(){
+    int date;
+    cout<<"enter date ";
+    cin>>date;
+    if (date>0){
+        if (date<32){
+            cout<<"date is valid";
+        }
+    }
+    return 0;
+}
+
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int date;
+    cout<<"enter date ";
+    cin>>date;
+    if (date>0){
+        if (date<32){
+            cout<<"date is valid"<<endl;
+            if (date ==25){
+                cout<<"hbd";
+            }
+        }
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
