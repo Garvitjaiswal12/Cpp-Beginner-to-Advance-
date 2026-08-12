5641875bcc0095cadad11a2e751fed5077dfde44
Garvit Jaiswal <garvitjaiswal12@gmail.com>
2026-08-12 23:51:52 +0530

    Add while loop to print numbers from 1 to 10

---
+++ whileloop.cpp
@@ -0,0 +1,12 @@
+// Jab tak ye conditon achieve nhi hogi tab tak loop chlega
+  #include <iostream>
+using namespace std;
+int main(){
+   int counter=1;
+   while(counter<=10){
+    cout<<counter<<" garvit "<<endl;
+    counter ++;
+   }
+    return 0;
+}
+
