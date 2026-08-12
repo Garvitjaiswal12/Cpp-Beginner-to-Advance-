d6e5dae5ec2530f7187df84a223de77d14dec3bd
Garvit Jaiswal <garvitjaiswal12@gmail.com>
2026-08-12 19:08:49 +0530

    Create shorthandoperator.cpp

---
+++ shorthandoperator.cpp
@@ -0,0 +1,24 @@
+// % modulous operator is for finding remainder only for integers
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 3;
+    cout << a % b;
+    return 0;
+}
+// if we have float datatype then use fmod() of cmath library
+#include <iostream>
+ # include <cmath>
+using namespace std;
+int main() {
+    int a=10;
+    float b=3;
+    cout<<fmod(a,b);
+    return 0;
+}
+
+
+
+
