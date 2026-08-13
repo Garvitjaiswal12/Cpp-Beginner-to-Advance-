6dd63aba1ef62a539d400ba55c3ba6dc08860fcc
Garvit Jaiswal <garvitjaiswal12@gmail.com>
2026-08-13 10:21:10 +0530

    Create forloop.cpp

---
+++ forloop.cpp
@@ -0,0 +1,23 @@
+/* for(intialisations;condition;updation){
+           statements
+               }
+*/
+#include <iostream>
+using namespace std;
+int main(){
+ for(int counter=1;counter<=30;counter ++){
+    cout<<counter<<endl;
+ }
+ return 0;
+}
+
+
+// infinite loop
+#include <iostream>
+using namespace std;
+int main(){
+ for(;;){
+    cout<<"garvit"<<endl;
+ }
+ return 0;
+}
