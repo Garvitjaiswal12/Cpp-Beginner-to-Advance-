df4c178dbdd026c14063e431caa38481993a4043
Garvit Jaiswal <garvitjaiswal12@gmail.com>
2026-08-14 11:21:55 +0530

    Create break&continue.cpp

---
+++ break&continue.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int counter;
+    for (counter=1;counter<=5;counter++){
+        int innercounter;
+        for (innercounter=1;innercounter<=3;innercounter++){
+            cout<<innercounter<<endl;
+            if (innercounter==2){
+                break;
+            }
+        }
+    }
+ return 0; 
+}
+// continue: skips the current iteration and then continue with next iteration after performing updatation and check
+
+#include <iostream>
+using namespace std;
+int main(){
+    int counter;
+    for (counter=1;counter<=10;counter++){
+        if (counter==5){
+            continue;
+        }
+        cout<<counter<<endl;
+    }
+}
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int counter =1;
+    while(counter<=10){
+        if (counter==5){
+            counter++;
+            continue;
+        }
+        cout<<counter<<endl;
+        counter++;
+        
+    }
+    return 0;
+}
+
+
