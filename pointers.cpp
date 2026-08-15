09586973705237fb24fbd2b655e1ea35ffe31c8f
Garvit Jaiswal <garvitjaiswal12@gmail.com>
2026-08-15 08:31:06 +0530

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -21,3 +21,67 @@ int main(){
 // 1-->1
 
 
+// passing array to a function:
+#include <iostream>
+using namespace std;
+void show(int[],int);
+int main(){
+    int numbers[]={10,20,30,40,50};
+    int length=5;
+    show(numbers,length);
+    return 0;
+}
+void show(int array[],int length){
+    for(int counter=0;counter<length;counter++){
+        cout<<array[counter]<<endl;
+    }
+}
+// pass by refrence -- we are passing a memory address to a function
+#include <iostream>
+using namespace std;
+void display(int *ptr);
+int main(){
+    int age=24;
+    display(&age);
+    cout<<age<<endl;
+    return 0;
+}
+
+void display(int *ptr){
+    cout<<ptr<<endl;
+    cout<<*ptr<<endl;
+    *ptr=100;
+}
+
+
+// ARRay,pointer relationship
+#include <iostream>
+using namespace std;
+void display(int *ptr);
+int main(){
+    int numbers[5] ={29,31,12,55,25};
+    cout<<numbers[2]<<endl;
+    cout<<numbers<<endl;
+    cout<<*numbers<<endl;
+    cout<<*(numbers+2)<<endl;
+    return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+void display(int *ptr);
+int main(){
+    int numbers[5] ={29,31,12,55,25};
+    cout<<numbers[2]<<endl;
+    cout<<numbers<<endl;
+    cout<<*numbers<<endl;
+    cout<<*(numbers+2)<<endl;
+    return 0;
+}
+
+// 12
+// 0xffffe49d0b68
+// 29
+// 12
+
